8328399766d7e6062ec867e78c3b8ffc5900ff2e
Ciaran Wood <cyrzinsomnia@gmail.com>
2018-10-15 21:55:25 +0200

    remove all scenes apart from physicsSample

--- App.js
+++ App.js
@@ -28,44 +28,18 @@ var createReactClass = require('create-react-class');
 var apiKey = "YOUR_API_KEY_HERE";
 
 var vrScenes = {
-    '360PhotoTour': require('./js/360PhotoTour/MainScene'),
-    'HumanBody': require('./js/HumanBody/MainScene'),
-    'ProductShowcase': require('./js/ProductShowcase/ProductShowcase'),
-    'ViroMediaPlayer': require('./js/ViroMediaPlayer/ViroTheatre'),
-    'ParticleEmitters': require('./js/ParticleEmitters/ViroParticleTemplates'),
     'PhysicsSample': require('./js/PhysicsSample/BasicPhysicsSample'),
 }
 
-var arScenes = {
-  'ARSimpleSample': require('./js/ARSample/HelloWorldSceneAR.js'),
-  'ARPhysicsSample': require('./js/ARPhysicsSample/BasicPhysicsSample.js'),
-  'ARCarDemo' : require('./js/ARCarDemo/ARCarDemo.js'),
-  'ARPosterDemo' : require('./js/ARPosterDemo/ARPosterDemo.js'),
-}
-
-var showARScene = false;
-
 var ViroCodeSamplesSceneNavigator = createReactClass({
   render: function() {
-
-    if (showARScene) {
-      return (
-        <ViroARSceneNavigator
-          initialScene={{
-            scene: arScenes['ARSimpleSample'],
-          }}
-          apiKey={apiKey} />
-        );
-    } else {
       return (
         <ViroVRSceneNavigator
           initialScene={{
-            scene: vrScenes['360PhotoTour'],
+            scene: vrScenes['PhysicsSample'],
           }}
           apiKey={apiKey} />
       );
-
-    }
   }
 });
 
